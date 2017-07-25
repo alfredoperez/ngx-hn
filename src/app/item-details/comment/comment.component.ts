eda571b32b60bffc202cf407e7d0124f209ae445
Alfredo Perez <alfredo.perez.q@gmail.com>
2017-07-25 07:25:41 -0500

    fixed comment

--- src/app/item-details/comment/comment.component.ts
+++ src/app/item-details/comment/comment.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Comment } from '../../core/data/models/comment';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.css']
